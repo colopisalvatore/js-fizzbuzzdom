ab0538e8eeebdc4aa10183b6e4ad097bf4e1dfa6
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-10-10 17:46:07 +0200

    bonus 1 + bonus 2

--- js/script.js
+++ js/script.js
@@ -5,43 +5,41 @@
 // BONUS 1: Crea un container nel DOM, aggiungendo(attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 // BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
-//Creo un loop che va da 0 a 100
-// for (let num = 0; num <= 100; num++) {
-//     /**All'aumentare del conteggio, se il numero è divisibile per 3 e divisibile per 5 stampo in consolle FizzBuzz, ho unito il numero con FizzBuzz per chiarezza.
-//     **/
-//     if ((0 == num % 3) && (0 == num % 5)) {
-//         console.log("FizzBuzz" + " " + num);
-//         //Se il numero è divisibile per 5 stampiamo Buzz
-//     } else if (0 == num % 5) {
-//         console.log("Buzz" + " " + num);
-//         //Se il numero è divisibile per 3 stampiamo Fizz
-//     } else if (0 == num % 3) {
-//         console.log("fizz" + " " + num);
-//     } else {
-//         //nel frattempo, stampa ancora tutti i numeri che non soddisfano le condizioni sopra
-//         console.log(num);
-//     }
-// }
-
+let myContainer = document.getElementById("fizz");
 
 //Creo un loop che va da 0 a 100
 for (let i = 1; i <= 100; ++i) {
-    const div3 = i % 3 === 0;
-    const div5 = i % 5 === 0;
+    const myBox = document.createElement('div');    
+    myBox.className = 'box';
 
-    
-    if (div3 && div5) {
+    const div3 = i % 3 === 0;
+    const div5 = i % 5 === 0;    
+
         /**All'aumentare del conteggio, se il numero è divisibile per 3 e divisibile per 5 stampo in consolle FizzBuzz, ho unito il numero con FizzBuzz per chiarezza.
-     **/
+        **/
+    if (div3 && div5) {        
         console.log(`FizzBuzz`);
-    } else if (div3) {
+        myBox.className += ' fizzbuzz';
+        myBox.append('fizzbuzz');    
+    } 
         //Se il numero è divisibile per 3 stampiamo Fizz
+    else if (div3) {
         console.log(`Fizz`);
-    } else if (div5) {
-        //Se il numero è divisibile per 5 stampiamo Buzz
-        console.log(`Buzz`);
-    } else {
-         //nel frattempo, stampa ancora tutti i numeri che non soddisfano le condizioni sopra
-        console.log(i);
+        myBox.className += ' fizz';
+        myBox.append('fizz'); 
     }
+        //Se il numero è divisibile per 5 stampiamo Buzz
+    else if (div5) {
+        console.log(`Buzz`);
+        myBox.className += ' buzz';
+        myBox.append('buzz'); 
+    } 
+        //nel frattempo, stampa ancora tutti i numeri che non soddisfano le condizioni sopra    
+    else {
+         
+        console.log(i);
+        myBox.append(i);
+    }
+
+    myContainer.append(myBox);
 }
